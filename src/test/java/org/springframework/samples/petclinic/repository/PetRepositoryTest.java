26dfbf09d96f960c8f902d0c5d0be196bc9db7ba
Sebastián Millán <sebastian.millanordonez2019@gmail.com>
2024-01-17 10:55:52 +0100

    Test findPetType completado

---
+++ src/test/java/org/springframework/samples/petclinic/repository/PetRepositoryTest.java
@@ -0,0 +1,54 @@
+package org.springframework.samples.petclinic.repository;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.samples.petclinic.model.PetType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles({"postgresql","spring-data-jpa"})
+@Testcontainers
+@Sql(value = "classpath:db/postgresql/initDB.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "classpath:test/insert-pet.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+//@Sql(value = "classpath:test/delete-pet.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+class PetRepositoryTest {
+
+    @Autowired
+    PetRepository springDataPetRepository;
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer postgres = new PostgreSQLContainer(DockerImageName.parse("postgres:16-alpine"))
+        .withUsername("testUser")
+        .withPassword("testSecret")
+        .withDatabaseName("testDatabase");
+
+
+    @Test
+    void findPetTypes() {
+        List<PetType> resultado = springDataPetRepository.findPetTypes();
+        assertFalse(resultado.isEmpty());
+        assertEquals(2, resultado.size());
+        assertEquals("cat", resultado.get(0).getName());
+
+    }
+
+    @Test
+    void delete() {
+    }
+}
---
+++ src/test/resources/test/delete-pet.sql
@@ -0,0 +1,4 @@
+delete from types;
+delete from owners;
+delete from pets;
+delete from visits;
